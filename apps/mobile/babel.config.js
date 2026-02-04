82b7b1400631f5104cb62a7dbc3b7ec0793ac21c
kiddicoder <kristinnolafur9@gmail.com>
2026-02-04 13:02:15 +0000

    fix: explicitly enable expo router babel transform

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -1,6 +1,11 @@
+const { expoRouterBabelPlugin } = require("babel-preset-expo/build/expo-router-plugin");
+
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"]
+    presets: ["babel-preset-expo"],
+    // In npm workspaces, expo-router may not be resolvable from the root node_modules,
+    // so we add the router transform explicitly.
+    plugins: [expoRouterBabelPlugin]
   };
 };
